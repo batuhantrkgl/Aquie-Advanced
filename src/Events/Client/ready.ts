4e980e372a2e32dbeedf86cc68d8e23f1f5cf726
Aquie <89739171+aquieover0@users.noreply.github.com>
2022-03-25 14:54:18 +0300

    Bot Presence

--- src/Events/Client/ready.ts
+++ src/Events/Client/ready.ts
@@ -3,4 +3,10 @@ import { Event } from "../../Structures/Event";
 
 export default new Event("ready",() => {
     client.log("Ready");
+
+    //Bot Presence
+    client.user.setActivity({name: "137", type: "LISTENING"});
+    client.user.setStatus("dnd");
+    
+
 })
