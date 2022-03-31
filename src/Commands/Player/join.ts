f527a00c94e71fe90cd1f6758d29ebd181558f50
Aquie <89739171+aquieover0@users.noreply.github.com>
2022-04-01 02:10:00 +0300

    new Command | /join

---
+++ src/Commands/Player/join.ts
@@ -0,0 +1,24 @@
+import { player } from "../..";
+import { Embed } from "../../Functions/Embed";
+import { Command } from "../../Structures/Command";
+import { Queue } from "../../Structures/Queue";
+
+
+export default new Command({
+    name: "join",
+    description: "Connects to Voice Channel",
+    permissions: "ManagePlayer",
+    voiceChannel:true,
+    run: ({ interaction }) =>{
+
+        let queue:Queue = player.getQueue(interaction.guild);
+        if(queue == null) queue = player.createQueue(interaction.guild, {
+            textChannel: interaction.channel
+        });
+
+        queue.connect(interaction.member.voice.channel);
+
+        interaction.followUp({embeds: [Embed(`Connected to Channel  \`\` ${interaction.member.voice.channel.name} \`\``, 1)]});
+
+    }
+})
